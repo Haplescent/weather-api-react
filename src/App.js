3707f1a9ca4004cc99413545912b701eba2d14b0
John Merritt <johnmerritt95@gmail.com>
2020-05-09 17:08:23 -0700

    textbox can now change WebsiteClass Change State

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SearchAppBar from './components/SearchAppBar';
-import TemporaryDrawer from './components/TemporaryDrawer';
-import WeatherCard from './components/WeatherCard';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SearchAppBar from "./components/SearchAppBar";
+import TemporaryDrawer from "./components/TemporaryDrawer";
+import WeatherCard from "./components/WeatherCard";
+import WebsiteClass from "./components/WebsiteClass";
 
 function App() {
   return (
-    <div className="App">
-      {SearchAppBar()}
-      <WeatherCard />
+    <div>
+      <WebsiteClass />
     </div>
   );
 }
 
 export default App;
+
+{
+  /* <div className="App">
+<SearchAppBar />
+<WeatherCard />
+</div> */
+}
--- src/components/WebsiteClass.js
+++ src/components/WebsiteClass.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SearchAppBar from './SearchAppBar';
-import TemporaryDrawer from './TemporaryDrawer';
-import WeatherCard from './WeatherCard';
+import React from "react";
+import logo from "../logo.svg";
+import "../App.css";
+import SearchAppBar from "./SearchAppBar";
+import TemporaryDrawer from "./TemporaryDrawer";
+import WeatherCard from "./WeatherCard";
 
-export default class SpacingGridClass extends React.Component {
+export default class WebsiteClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        textInput = ""
+      textInput: "",
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
+    console.log("handleChange function working");
     this.setState({
-        textInput: event.target.value
-    // console.log(event)
-  })
+      textInput: event.target.value,
+    });
+  }
 
   render() {
+    console.log(this.state.textInput);
     return (
-        <div className="App">
-          <SearchAppBar handleChange={this.handleChange}/>
-          <WeatherCard />
-        </div>
-      );
+      <div className="App">
+        <SearchAppBar handleChange={this.handleChange} />
+        <WeatherCard />
+      </div>
+    );
   }
 }
