79ef3347d536dd1adaa2fcee6401a2b30a5b05c3
John Merritt <john_merritt@JohnMerrittiMac.local>
2020-05-13 14:20:52 -0700

    creating v2 branch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import SearchAppBar from "./components/SearchAppBar";
-import TemporaryDrawer from "./components/TemporaryDrawer";
-import WeatherCard from "./components/WeatherCard";
-import WebsiteClass from "./components/WebsiteClass";
+
 
 function App() {
-  return (
-    <div>
-      <WebsiteClass />
-    </div>
-  );
 }
 
 export default App;
 
-{
-  /* <div className="App">
-<SearchAppBar />
-<WeatherCard />
-</div> */
-}
