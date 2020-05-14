981ddf8a0e9ddaf512641ee68b636413ad8e4b2e
John Merritt <john_merritt@Nandhinis-iMac.targetdiscovery.lan>
2020-05-14 15:55:38 -0700

    edited readme

--- src/App.js
+++ src/App.js
@@ -4,16 +4,37 @@ import './App.css';
 import SearchAppBar from './components/SearchAppBar';
 import TemporaryDrawer from './components/TemporaryDrawer';
 import WeatherCard from './components/CurrentWeather';
+import SearchForm from './components/SearchForm'
 
 
-function App() {
-    return (
-      <div className="App">
-        {SearchAppBar()}
-        <WeatherCard />
-      </div>
-    );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textInput: "",
+      currentWeather: {},
+      fiveDayForecast: {},
+      searchHistory: []
+    };
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-export default App;
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log("handleSubmit button in App class component working")
+    console.log(event.target.)
+    let historyToPush = []
+    historyToPush.push(event.target.value)
+    this.setState({
+      textInput: event.target.value,
+      searchHistory: historyToPush
+    })
+  }
 
+  render() {
+    console.log(this.state.searchHistory)
+    return (<div>
+      <SearchForm handleClick={this.handleSubmit}/>
+    </div>)
+  }
+}
