6172ed0882e245f34cf0bf5809664868870b7887
John Merritt <johnmerritt95@gmail.com>
2020-05-14 18:02:14 -0700

    onSubmit Search Form Working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SearchAppBar from './components/SearchAppBar';
-import TemporaryDrawer from './components/TemporaryDrawer';
-import WeatherCard from './components/CurrentWeather';
-import SearchForm from './components/SearchForm'
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SearchAppBar from "./components/SearchAppBar";
+import TemporaryDrawer from "./components/TemporaryDrawer";
+import WeatherCard from "./components/CurrentWeather";
+import SearchForm from "./components/SearchForm";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,27 +13,28 @@ export default class App extends React.Component {
       textInput: "",
       currentWeather: {},
       fiveDayForecast: {},
-      searchHistory: []
+      searchHistory: [],
     };
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    console.log("handleSubmit button in App class component working")
-    console.log(event.target.)
-    let historyToPush = []
-    historyToPush.push(event.target.value)
+  handleSubmit(textToHandle) {
+    // event.preventDefault();
+    console.log("handleSubmit button in App class component working");
+    let historyToPush = [];
+    historyToPush.push(textToHandle);
     this.setState({
-      textInput: event.target.value,
-      searchHistory: historyToPush
-    })
+      textInput: textToHandle,
+      searchHistory: historyToPush,
+    });
   }
 
   render() {
-    console.log(this.state.searchHistory)
-    return (<div>
-      <SearchForm handleClick={this.handleSubmit}/>
-    </div>)
+    console.log(this.state.searchHistory);
+    return (
+      <div>
+        <SearchForm handleClick={this.handleSubmit} />
+      </div>
+    );
   }
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React from "react";
 
 export default class SearchForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: ""
-        }
-        this.handleChange = this.handleChange.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleChange(event) {
-        event.preventDefault();
-        this.setState({value: event.target.value,
-        });
-    }
+  handleChange(event) {
+    event.preventDefault();
+    console.log(event.target.value);
+    this.setState({ value: event.target.value });
+  }
 
-    render() {
-        return (
-          <form onSubmit={this.props.handleClick}>
-            <label>
-              Search:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
-        );
-      }
+  handleSubmit(event) {
+    event.preventDefault();
+    let eventObject = event.target.getAttribute("value");
+    console.log("handleSubmit is working");
+    console.log(eventObject);
+    this.props.handleClick(eventObject);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} value={this.state.value}>
+        <label>
+          Search:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
