4549ba753cea922e7079bceff6f6f1c08acab92b
John Merritt <johnmerritt95@gmail.com>
2020-05-15 21:46:31 -0700

    onclick recent search history will research

--- src/components/FiveDayForecast.js
+++ src/components/FiveDayForecast.js
@@ -3,12 +3,10 @@ import React from "react";
 export default function FiveDayForecast(props) {
   let forecastArray = props.forecastArray;
   let html;
-  //   console.log(weather);
-  //   console.log(weather === undefined);
   if (forecastArray.length === 0) {
     html = <h1>Five Day Forcast here</h1>;
   } else {
-    html = <h1>{forecastArray[0].clouds}</h1>
+    html = <h1>{forecastArray[0].clouds}</h1>;
   }
 
   return html;
--- src/components/PreviousSearchHistory.js
+++ src/components/PreviousSearchHistory.js
@@ -2,17 +2,40 @@ import React from "react";
 
 import TemporaryDrawer from "./TemporaryDrawer.js";
 
-export default function PreviousSearchHistory(props) {
-  let html = <div></div>;
-  if (props.searchHistory.length > 0) {
-    let array = props.searchHistory.map((text) => (
-      <button onclick="" value={text}>
-        {text}
-      </button>
-    ));
-    console.log(array);
-    return array;
-  } else {
-    return <h1>{props.searchHistory}</h1>;
+export default class PreviousSearchHistory extends React.Component {
+  constructor(props) {
+    console.log("Mounting PreviousSearchHistory");
+    super(props);
+    this.state = {
+      textInput: "",
+      currentWeather: {},
+      fiveDayForecast: [],
+      searchHistory: [],
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    console.log(this);
+    event.preventDefault();
+    let eventObject = event.target.getAttribute("value");
+    console.log("handleSubmit is working");
+    console.log(eventObject);
+    this.props.handleClick(eventObject);
+  }
+
+  render() {
+    let html = <div></div>;
+    if (this.props.searchHistory.length > 0) {
+      let array = this.props.searchHistory.map((text) => (
+        <button onClick={this.handleSubmit} value={text}>
+          {text}
+        </button>
+      ));
+      console.log(array);
+      return array;
+    } else {
+      return <h1>{this.props.searchHistory}</h1>;
+    }
   }
 }
